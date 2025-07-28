ce8a2532db25b5780ddfe2422b0361d1e3bf45d5
agustin.agustinmealla <agustin.agustinmealla@gmail.com>
2025-07-28 18:25:08 +0000

    aa

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Assuming you have an index.css for global styles
+import { BrowserRouter } from 'react-router-dom';
+import './theme.css'; // CORRECCIÓN: Usar el archivo de tema correcto.
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
